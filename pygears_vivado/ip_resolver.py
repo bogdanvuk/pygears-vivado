2fda6a4fcc1552edb6244deddc7fdf97ca0b95b9
Bogdan Vukobratovic <bogdan.vukobratovic@gmail.com>
2020-05-21 21:03:01 +0200

    Addition settings in cordic

--- pygears_vivado/ip_resolver.py
+++ pygears_vivado/ip_resolver.py
@@ -23,7 +23,9 @@ class IPResolver(ResolverBase):
 
     @property
     def file_basename(self):
-        return self.files[0]
+        for f in self.files:
+            if os.path.basename(f).startswith(self.module_name):
+                return f
 
     @property
     def files(self):
@@ -40,31 +42,6 @@ class IPResolver(ResolverBase):
     @property
     def impl_path(self):
         return find_in_dirs(self.file_basename, self.hdl_path_list)
-
-    @property
-    @functools.lru_cache()
-    def impl_parse(self):
-        if self.impl_path:
-            with open(self.impl_path, 'r') as f:
-                return parse(f.read())
-
-        return None
-
-    @property
-    def impl_params(self):
-        return self.impl_parse[-1]
-
-    @property
-    def impl_intfs(self):
-        parse_res = self.impl_parse()
-        if parse_res:
-            intfs = {}
-            for i in parse_res[2]:
-                intfs[i['name']] = i
-
-            return intfs
-
-        return None
 
     @property
     def params(self):
--- pygears_vivado/lib/cordic.py
+++ pygears_vivado/lib/cordic.py
@@ -11,18 +11,21 @@ from pygears.core.gear import InSig
 @gear(hdl={'hdlgen_cls': SVVivModuleInst},
       sigmap={'aclk': 'clk'},
       signals=[InSig('aclk', 1)])
-async def cordic(s_axis_phase: Fixp[3, 'W'],
-                 *,
-                 output_width=b'input_width',
-                 input_width=b'W',
-                 functional_selection="Sin_and_Cos",
-                 phase_format="Scaled_Radians",
-                 flow_control="Blocking",
-                 out_tready=True,
-                 data_format="SignedFraction",
-                 _w_out=b'((W+7)//8)*8') -> {
-                     'm_axis_dout': Tuple[Fixp[2, '_w_out'], Fixp[2, '_w_out']]
-                 }:
+async def cordic(
+        s_axis_phase: Fixp[3, 'W'],
+        *,
+        output_width=b'input_width',
+        input_width=b'W',
+        functional_selection="Sin_and_Cos",
+        architectural_configuration="Word_Serial",  # Word_Serial, Parallel
+        pipelining_mode="Optimal",  # Optimal, Maximum
+        phase_format="Scaled_Radians",
+        flow_control="Blocking",  # NonBlocking, Blocking
+        out_tready=True,
+        data_format="SignedFraction",
+        _w_out=b'((W+7)//8)*8') -> {
+            'm_axis_dout': Tuple[Fixp[2, '_w_out'], Fixp[2, '_w_out']]
+        }:
     async with s_axis_phase as p:
         yield (Fixp[2, output_width](math.cos(math.pi * float(p))),
                Fixp[2, output_width](math.sin(math.pi * float(p))))
